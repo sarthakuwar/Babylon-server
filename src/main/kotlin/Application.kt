b19e8ec964a7253578e3b6ab8bb05d20f75be50e
sarthakkuwar <kuwarsarthak711@gmail.com>
2025-07-27 19:38:44 +0530

    main auth level 1

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,15 +1,21 @@
 package com.example
 
+ import com.example.db.DatabaseFactory
+ import com.example.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
 }
 
+// This is the main entry point of the application.
+// It initializes the database and configures the Ktor plugins.
 fun Application.module() {
-    configureFrameworks()
+     DatabaseFactory.init()
     configureSerialization()
-    configureMonitoring()
     configureSecurity()
     configureRouting()
 }
